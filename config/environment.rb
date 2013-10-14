e5d3df958e15c0f4b2ec7b38ebf20c30995f1891
Yves Accad <yaccad@gmail.com>
2013-10-14 16:25:34 -0700

    environment change

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,10 @@ Secushare::Application.initialize!
 
 #Formatting DateTime to look like "10/20/2011 10:28PM"  
 Time::DATE_FORMATS[:default] = "%m/%d/%Y %l:%M%p"
+
+module OpenSSL
+  module SSL
+    remove_const :VERIFY_PEER
+  end
+end
+OPENSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
